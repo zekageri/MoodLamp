7d89e382aeaf1dc5f1589c3a5d0d8b5ab45015e0
roorsolya <73881005+roorsolya@users.noreply.github.com>
2020-11-07 15:36:44 +0100

    background

--- Web_Test/FrontEnd/Index.js
+++ Web_Test/FrontEnd/Index.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
     console.log("A JS file Jqueryvel innentől kezdődik. Ez onpageload-kor történik!");
 });
+$(function() {
+    $('.btn-group-fab').on('click', '.btn', function() {
+      $('.btn-group-fab').toggleClass('active');
+    });
+    $('has-tooltip').tooltip();
+  });
+  
