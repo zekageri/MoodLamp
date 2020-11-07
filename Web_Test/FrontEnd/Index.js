43d607f28d6d34a6f99ee4f187465d894b8b0357
roorsolya <73881005+roorsolya@users.noreply.github.com>
2020-11-07 18:47:30 +0100

    nn

--- Web_Test/FrontEnd/Index.js
+++ Web_Test/FrontEnd/Index.js
@@ -1,28 +1,27 @@
-  $( document ).ready(function() {
-    Init_FAB();
-    Menu_System_Init();
-  });
-  
-  var Page_Before = "Home";
-  function Init_FAB(){
-    $('.btn-group-fab').on('click', '.btn', function() {
-      $('.btn-group-fab').toggleClass('active');
-    });
-    $('has-tooltip').tooltip();
-  }
+$( document ).ready(function() {
+  Init_FAB();
+  Menu_System_Init();
+});
 
-  function Menu_System_Init(){
-    $( ".menutrigg" ).click(function() {
-        GoToPage( $(this).attr("title") );
-    });
-  }
-  function GoToPage(Page){
-      if(Page != Page_Before){
-         //$("a[title*='"+ Page_Before +"']").removeClass("active");
-          $("#"+Page).show();
-          $("#"+Page_Before).hide();
-          //$("a[title*='"+ Page +"']").addClass("active");
-          Page_Before = Page;
-      }
-  }
-  
+var Page_Before = "Home";
+function Init_FAB(){
+  $('.btn-group-fab').on('click', '.btn', function() {
+    $('.btn-group-fab').toggleClass('active');
+  });
+  $('has-tooltip').tooltip();
+}
+
+function Menu_System_Init(){
+  $( ".menutrigg" ).click(function() {
+      GoToPage( $(this).attr("title") );
+  });
+}
+function GoToPage(Page){
+    if(Page != Page_Before){
+       //$("a[title*='"+ Page_Before +"']").removeClass("active");
+        $("#"+Page).show();
+        $("#"+Page_Before).hide();
+        //$("a[title*='"+ Page +"']").addClass("active");
+        Page_Before = Page;
+    }
+}
